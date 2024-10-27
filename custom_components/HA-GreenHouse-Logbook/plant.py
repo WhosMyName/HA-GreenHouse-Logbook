299844c370a3692a7837bc00663083b2923cc191
WhosMyName <chikangwu95@gmail.com>
2024-10-28 00:27:35 +0100

    added imports an thoughts (and prayers)

--- custom_components/HA-GreenHouse-Logbook/plant.py
+++ custom_components/HA-GreenHouse-Logbook/plant.py
@@ -1,7 +1,40 @@
-    """_summary_
-    """
+"""_summary_
+"""
 
 # https://developers.home-assistant.io/docs/core/entity
 
-from homeassistant.components.camera.Camera import CameraEntity
+from homeassistant.components.camera import CameraEntity
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.datetime import DateTimeEntity
+from homeassistant.components.text import TextEntity, TextEntityDescription
+from homeassistant.components.todo import TodoListEntity, TodoItem, TodoItemStatus
+from homeassistant.components.alarm_control_panel import AlarmControlPanelEntity, AlarmControlPanelEntityDescription, AlarmControlPanelEntityFeature, AlarmControlPanelState
 
+
+# thoughts:
+
+# CameraEntity:
+# to select a image-capturing input device
+# and use shome whacky bounding box to select specific plants and zoom into bbox
+# daily images of plants
+# maybe timelapses
+
+# SensorEntity:
+# for Humidity, AirQuality, Temperature, Pressure, EC, pH, ....
+
+# DateTimeEntity
+# to set dates (????)
+
+# TextEntity:
+# [in hopes MD can be used]
+# to enter extra info, e.g. fertilizer usage/type, pest control, plant changes, etc....
+
+# TodoListEntity:
+# checkboxes for watering, fertilizing, pest control, trimming
+
+# AlarmControlPanelEntity:
+# set timeframe and alarm the user if plant hasn't been watered within of the timeframe
+
+
+class Plant(CameraEntity, ):
+    pass
